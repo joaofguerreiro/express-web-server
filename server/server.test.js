3833ef995f6f32ae5f2cfa23096b1131904a3f97
João Guerreiro <joaofcguerreiro@gmail.com>
2019-02-20 11:13:36 +0000

    Reorganized unit tests using describe function

--- server/server.test.js
+++ server/server.test.js
@@ -1,2 +1,38 @@
 const request = require('supertest');
-const express = require('express');
+const expect = require('expect');
+var app = require('./server.js').app;
+
+// Testing express endpoints
+describe('Server', () => {
+    
+    describe('GET /error', () => {
+        it('should return 404 response', (done) => {
+            request(app)
+            .get('/error')
+            .expect({
+                error: 'Object not found'
+            })
+            .expect((res) => {
+                expect(res.body).toIncludeKey('error');
+            })
+            .expect(404)
+            .end(done);
+        });
+    });
+
+    describe('GET /users', () => {
+        it('should return my user', (done) => {
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    name: 'João',
+                    age: 26
+                })
+            })
+            .end(done);
+        });
+    });
+
+});
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -7,50 +7,56 @@ const utils = require('./utils.js');
  * 
  */
 
-// Behavior-driven development (BDD)
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
-
-    expect(res).toBe(44);
-    expect(res).toBeA('number');
-});
-
-it('should square a number', () => {
-    var res = utils.square(4);
-
-    expect(res).toBe(16);
-    expect(res).toBeA('number');
-})
-
-it('should expect some values', () => {
-    expect(12).toBe(12);
-    expect({name: 'João'}).toNotEqual({name: 'joão'});
-    expect([1, 2, 3]).toInclude(1);
-    expect([1, 2, 3]).toExclude(5);
-    expect({name: 'João', age: 26}).toInclude({age: 26});
-});
-
-it('should set firstName and lastName', () => {
-    var user = {location: 'Faro', age: 26};
-    res = utils.setName(user, 'João Guerreiro');
-
-    expect(user).toEqual(res);  // objects are passed by reference so user and res have the same value
-    expect(res).toIncludeKeys(['firstName', 'lastName']);
-    expect(res.firstName).toBe('João');
-    expect(res.lastName).toBe('Guerreiro');
-});
-
-// Should pass a done argument when testing async functions
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(2, 3, (sum) => {
-        expect(sum).toBe(5).toBeA('number');
-        done();
+describe('Utils', () => {
+    
+    describe('#add', () => {
+        // Behavior-driven development (BDD)
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+        
+            expect(res).toBe(44);
+            expect(res).toBeA('number');
+        });
     });
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(4, (square) => {
-        expect(square).toBe(16).toBeA('number');  // chaining multiple tests is possible
-        done();
+    
+    it('should square a number', () => {
+        var res = utils.square(4);
+    
+        expect(res).toBe(16);
+        expect(res).toBeA('number');
+    })
+    
+    it('should expect some values', () => {
+        expect(12).toBe(12);
+        expect({name: 'João'}).toNotEqual({name: 'joão'});
+        expect([1, 2, 3]).toInclude(1);
+        expect([1, 2, 3]).toExclude(5);
+        expect({name: 'João', age: 26}).toInclude({age: 26});
     });
+    
+    it('should set firstName and lastName', () => {
+        var user = {location: 'Faro', age: 26};
+        res = utils.setName(user, 'João Guerreiro');
+    
+        expect(user).toEqual(res);  // objects are passed by reference so user and res have the same value
+        expect(res).toIncludeKeys(['firstName', 'lastName']);
+        expect(res.firstName).toBe('João');
+        expect(res.lastName).toBe('Guerreiro');
+    });
+    
+    // Should pass a done argument when testing async functions
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(2, 3, (sum) => {
+            expect(sum).toBe(5).toBeA('number');
+            done();
+        });
+    });
+    
+    it('should async square a number', (done) => {
+        utils.asyncSquare(4, (square) => {
+            expect(square).toBe(16).toBeA('number');  // chaining multiple tests is possible
+            done();
+        });
+    });
+
 });
